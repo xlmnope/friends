779547e315ec3485b29fc96c522373696d64c582
xlmnope <lmgiroud@gmail.com>
2019-03-09 11:16:50 -0800

    change port to dynamic

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var createAPIRoutes = require('./app/routing/apiRoutes.js');
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
